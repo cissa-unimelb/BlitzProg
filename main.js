1578def7f0cc120c81fdbe9c18aa40c41201be3f
Keith Leonardo <kleonardo52@gmail.com>
2021-08-03 02:56:25 +1000

    Refactor code into modules

--- main.js
+++ main.js
@@ -6,17 +6,25 @@ if (process.env.NODE_ENV !== "production") {
 // Load modules
 const discord = require('discord.js');
 const mongoose = require('mongoose');
-const Sandbox = require("sandbox");
-const express = require('express');
-const http = require('http');
-const fs = require('fs');
 const Schema = require('./schema');
-const { response } = require("express");
+const Game = require("./game");
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
 
+// Initialize API for submitting problems
+const app = express();
+const server = http.createServer(app);
+const api = require("./api");
 
-// Create an instance of a Discord client and Sandbox
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/api", api);
+
+// Create an instance of a Discord client and the Game
 const client = new discord.Client();
-const sandbox = new Sandbox();
+const game = new Game();
 
 // Connect to the database
 mongoose.connect(
@@ -27,183 +35,81 @@ mongoose.connect(
      useFindAndModify: false,
    },
    (err) => {
-     if (err) {
-       console.log(err);
-     } else {
-       console.log("Connected to db!");
-       
-  
-     }
-   }
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log("Connected to db...");
+
+      // -- DEBUGGING TEST CODE START --
+      // // Add a problem to the database
+      // var problemObject = {
+      //   name: 'Problem Name',
+      //   description: 'Problem Description',
+      //   difficulty: "Easy",
+      //   status: "Pending",
+      //   cases: [{input: "1", output: "1", memoryLimitBytes: 1, timeLimitSeconds: 1}],
+      //   author: 'Problem Author',
+      //   verified: true
+      // };
+
+      // Schema.Problem.create(problemObject, function(err, result){
+      //   if (err) {
+      //     console.log(err);
+      //   } else {
+      //     console.log(result);
+      //   }
+      // });
+
+      // // Retrieve a problem from the database
+      // console.log('Hi');
+      // Schema.Problem.find({difficulty: "Easy"}, (err, data) =>{
+      //   if (err){
+      //     console.log(err);
+      //   } else {
+      //     console.log(data);
+      //   }
+      // })
+      // -- DEBUGGING TEST CODE END --
+
+      // Only after this the bot will start reacting to information received from Discord
+      client.on('ready', () => {
+        console.log('BlitzProg is online!');
+      });
+
+      // Handle messages sent
+      client.on('message', message => {
+        // Making sure that the author of the message is not a bot
+        if (message.author.bot) return false;
+
+        // Bot should only run in the BlitzProg channel
+        if (message.channel.id != '863230229488467978') return false; 
+
+        // Game commands
+        if (message.content == '!initiate'){
+          game.initiate(message);
+        }
+        if (message.content == '!join') {
+          game.join(message);
+        }
+        if (message.content == '!start') {
+          game.start(message);
+        }
+        if (message.content.startsWith('!submit')) {
+          game.submit(message);
+        }
+        if (message.content == '!end') {
+          game.end(message);
+        }
+      });
+
+      // Login to Discord
+      client.login(process.env.DISCORD_API_KEY);
+
+      // Assign a port to run our API
+      const port = 4040;
+      server.listen(process.env.PORT || port, () => {
+        console.log(`API server listening on port *:${port}`);
+      });
+    }
 );
-
-// Express app
-const app = express();
-var port = 3000;
-const router = express.Router();
-app.use("/", router);
-
-
-
-
-
-
-
-// Display HTML file 
-fs.readFile('./data-input.html', function (err, html){
- if (err) {
-   throw err;
- } 
- http.createServer(function(req, res){
-   res.writeHeader(200, {"Content-Type": "text/html"});
-   res.write(html);
-   res.end();
- }).listen(port);
-});
-
-
-
-
-
-
-// Only after this the bot will start reacting to information received from Discord
-client.on('ready', () => {
-   console.log('BlitzProg is online!');
-});
-
-
-// Tracks game status
-game_active = false;
-join_active = false;
-
-router.route("/add").post(function(req, res) {
-});
-
-// Event listener for messages
-client.on('message', message =>{
-
-
-
-
-
-
-
-
- // Add a problem to the database
-
- var problemObject = {
-   name: 'Problem Name',
-   description: 'Problem Description',
-   status: "Easy",
-   cases: [{input: "1", output: "1", memoryLimitBytes: 1, timeLimitSeconds: 1}],
-   author: 'Problem Author',
-   verified: true
- };            
-
- Schema.Problem.create(problemObject, function(err, result){
-   if (err) {
-     console.log(err);
-   } else {
-     console.log(result);
-   }
- }); 
-
- // Retrieve a problem from the database
-
- console.log('Hi');
- Schema.Problem.find({status: "Easy"}, (err, data) =>{
-   if (err){
-     console.log(err);
-   } else {
-     console.log(data);
-   }
- })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   // Making sure that the author of the message is not a bot
-   if (message.author.bot) return false;
-
-   // Bot should only run in the BlitzProg channel
-   if (message.channel.id != '863230229488467978') return false; 
-
-   // Get player role 
-   let playerRole = message.guild.roles.cache.find(role => role.name === "Player");
-
-
-   // Game initating process
-   if (message.content == '!initiate' && game_active == false){
-     message.channel.send('Initiating Game');
-     game_active = true;
-     join_active = true;
-
-     // Add current player to game
-     let member = message.member;
-     member.roles.add(playerRole).catch(console.error);
-
-   }
-
-   // Game joining process
-   if (join_active == true){
-     if (message.content == '!join'){
-       message.channel.send('Adding Player');
-       let member = message.member;
-       member.roles.add(playerRole).catch(console.error);
-     } else if (message.content == '!start'&& message.member.roles.cache.some(role => role.name === "Player")){
-       message.channel.send('Starting Game');
-       join_active = false;
-     }
-   }
-
-   // Game ending process
-   if (message.content == '!end' && game_active == true){
-     message.channel.send('Ending Game');
-     game_active = false;
-
-     // Remove all players from the game 
-     const role = message.guild.roles.cache.find(role => role.name === 'Player');
-     message.guild.roles.create({
-       data: {
-         name: role.name,
-         color: role.color,
-         hoist: role.hoist,
-         position: role.position,
-         permissions: role.permissions,
-         mentionable: role.mentionable
-       }         
-     })
-     role.delete('I had to')
-   }
-
-   // Only take in the input if it starts with the !submit command
-   if (message.content.startsWith('!submit')){
-       code = message.content.replace('!submit', '');
-       code = code.replace("```", "");
-       code = code.replace("```", "");
-       
-       // Output the submission details
-       message.channel.send('USER: ');
-       message.channel.send(message.member.user.tag);
-
-       // Sandbox the code submitted by the user
-       message.channel.send('OUTPUT: ');
-       sandbox.run(code, function(output){
-           message.channel.send(output.console);
-           console.log(output.console);
-       })
-   } 
-})
-
-client.login(process.env.DISCORD_API_KEY);
