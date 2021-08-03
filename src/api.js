c2c02dd5900689ffba965692eef823dfdb198554
Keith Leonardo <kleonardo52@gmail.com>
2021-08-03 17:20:24 +1000

    Fix API route for submitting problems

--- src/api.js
+++ src/api.js
@@ -5,24 +5,21 @@ const schema = require("./schema");
 /**
  * Submit a new problem with its test cases
  */
-router.post("submit", (req, res) => {
-    schema.TestCase.insertMany(req.body.testcases)
-        .then((testcases) => {
-            schema.Problem.insertMany([{
-                name: req.body.name,
-                description: req.body.description,
-                difficulty: req.body.difficulty,
-                status: "Pending",
-                cases: testcases,
-                author: req.body.author,
-                solution: req.body.solution
-            }])
-                .then((problem) => {
-                    res.send(problem);
-                })
-                .catch((error) => {
-                    res.status(500).send(error.message);
-                })
+router.post("/submit", (req, res) => {
+    schema.Problem.insertMany([{
+        name: req.body.name,
+        description: req.body.description,
+        difficulty: req.body.difficulty,
+        status: "Pending",
+        cases: req.body.testcases,
+        author: req.body.author.length ? req.body.author : "Anonymous",
+        solution: req.body.solution
+    }])
+        .then((problem) => {
+            res.send(problem);
+        })
+        .catch((error) => {
+            res.status(500).send(error.message);
         });
 });
 
